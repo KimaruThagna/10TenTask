352abc270a6ec81a218dc731d2d445fd612f124d
Kimaru thagana <macbuntu@localhost.localdomain>
2019-06-08 15:23:07 +0300

    sending to DB--logic

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse,reverse,HttpResponseRedirect
 from django.contrib import messages
+from django.db.models import F
 from .models import *
 import json
 # Create your views here.
@@ -19,6 +20,29 @@ def Home(request):
 def Purchase(request):
     if request.POST:
 
+        receivedData=dict(request.POST)
+        item=list(receivedData['item'])
+        qty = list(receivedData['qty'])
+
+        itemSetDictionary={}
+        for i in range (len(item)):
+            itemSetDictionary[item[i]]=qty[i]
+
+
+        # check if available in DB
+        for obj in itemSetDictionary:
+            if int(itemSetDictionary[obj]) > int(Product.objects.filter(name=obj).quantity):
+                messages.error(request,'Sorry but we are out of'+str(obj))
+                return HttpResponseRedirect(reverse("shop:home"))# out of any item of purchase
+            else:
+                pass
+        # all items are available. Update DB
+        for obj in itemSetDictionary:
+           Product.objects.filter(name=obj).update(quantity=F('quantity')-itemSetDictionary[obj])
+
+        # after update
+        # after update
+        messages.success(request,'Purchase complete')
         return HttpResponseRedirect(reverse("shop:confirm"))
     else:
         return HttpResponseRedirect(reverse("shop:home"))
